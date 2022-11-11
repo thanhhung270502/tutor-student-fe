7f275d27f2b20d8a7d396f92eff03105949b5d22
thanhhung270502 <thanhhung270502@gmail.com>
2022-11-12 01:15:37 +0700

    Update routes

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Home from './pages/Home/Home.js'
 import ClassList from './pages/ClassList/ClassList'
 import Login from './pages/Login'
 import SignUp from './pages/SignUp'
+import Personal_info from './pages/personal_info/index.js';
 
 function App() {
     return (
@@ -16,6 +17,7 @@ function App() {
                 <Route path="/classlist" element={<ClassList />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/signup" element={<SignUp />} />
+                <Route path="/personal_info" element={<Personal_info />} />
             </Routes>
         </Layout >
 
