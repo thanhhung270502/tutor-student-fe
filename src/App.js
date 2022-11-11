7a88e247dd76ed04d565a48cc43dd0d96f3edc40
thanhhung270502 <thanhhung270502@gmail.com>
2022-11-12 02:03:28 +0700

    1

--- src/App.js
+++ src/App.js
@@ -4,12 +4,9 @@ import Layout from '~/components/Layout/DefaultLayout/Layout';
 
 import Home from './pages/Home/Home.js'
 import ClassList from './pages/ClassList/ClassList'
-import SignUp from './pages/SignUp/index.js';
-import Createclass from './pages/CreateClass/index.js';
-
 import Login from './pages/Login'
-import Personal_info from './pages/personal_info/index.js';
-import ListTutor from './pages/ListTutor/index.js';
+import SignUp from './pages/SignUp'
+import PendingClass from './pages/PendingClass/PendingClass'
 
 function App() {
     return (
@@ -20,9 +17,9 @@ function App() {
                 <Route path="/classlist" element={<ClassList />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/signup" element={<SignUp />} />
-                <Route path="/create_class" element={<Createclass />} />
-                <Route path="/personal_info" element={<Personal_info />} />
-                <Route path="/tutorlist" element={<ListTutor />} />
+
+                // Admin
+                <Route path="/pendingClass" element={<PendingClass />} />
             </Routes>
         </Layout >
 
