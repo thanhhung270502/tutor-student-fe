742b2ea36a3320673e395090ed4d026e265279ed
Tieu Ngoc <thaingoctieu2002@gmail.com>
2022-11-11 10:20:04 +0700

    classlist page

--- src/pages/ClassList/ClassList.js
+++ src/pages/ClassList/ClassList.js
@@ -6,11 +6,15 @@ import style from './ClassList.module.css'
 import { Table } from 'react-bootstrap'
 import data from './ClassList.json'
 
+import Row from './Row'
+
 const ClassList = () => {
     const [classInfo, setClassInfo] = useState([]);
 
+    const [show, setShow] = useState(false);
+
     useEffect(() => {
-        /* use api
+        /* use api / wait until data is updated
         async () => {
             info = await getClassInfo();
             setClassInfo(info);
@@ -25,24 +29,17 @@ const ClassList = () => {
             <table className={`table table-borderless ${style.table}`}>
                 <thead className={style.theader}>
                     <tr>
-                        <th className={`col-md-2 ${style.th}`}>MÃ LỚP</th>
-                        <th className={`col-md-2 ${style.th}`}>MÔN</th>
-                        <th className={`col-md-2 ${clsx(style.th, style.center)}`}>LỚP</th>
+                        <th className={`col-md-3 ${style.th} ${style.hihi}`}>MÃ LỚP</th>
+                        <th className={`col-md-3 ${style.th}`}>MÔN</th>
+                        <th className={`col-md-3 ${clsx(style.th, style.center)}`}>LỚP</th>
                         <th className={`col-md-3 ${style.th}`}>NGÀY ĐĂNG KÝ</th>
-                        <th className={`col-md-3 ${style.th}`}></th>
+                        {/* <th className={`col-md-3 ${style.th}`}></th> */}
                     </tr>
                 </thead>
                 <tbody>
                     {
                         classInfo.map(
-                            (c, index) =>
-                                <tr key={index} className={style.tr}>
-                                    <td className={`col-md-2 ${clsx(style.td)}`}>{c.classID}</td>
-                                    <td className={`col-md-2 ${clsx(style.td)}`}>{c.subject}</td>
-                                    <td className={`col-md-2 ${clsx(style.td, style.center)}`}>{c.grade}</td>
-                                    <td className={`col-md-3 ${clsx(style.td)}`}>{c.date}</td>
-                                    <td className={`col-md-3 ${clsx(style.td)}`}></td>
-                                </tr>
+                            (c, index) => <Row key={index} data={classInfo[index]} c={c}/>
                         )
                     }
                 </tbody>
