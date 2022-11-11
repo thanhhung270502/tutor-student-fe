7faf7346f2cd38ad06255c465e9ec62a21ba8ae5
thanhhung270502 <thanhhung270502@gmail.com>
2022-11-12 02:05:20 +0700

    Update routes

--- src/App.js
+++ src/App.js
@@ -1,28 +1,32 @@
 import { Routes, Route } from 'react-router-dom';
-import React from 'react'
+import React from 'react';
 import Layout from '~/components/Layout/DefaultLayout/Layout';
 
-import Home from './pages/Home/Home.js'
-import ClassList from './pages/ClassList/ClassList'
-import Login from './pages/Login'
-import SignUp from './pages/SignUp'
-import PendingClass from './pages/PendingClass/PendingClass'
+import Home from './pages/Home/Home.js';
+import ClassList from './pages/ClassList/ClassList';
+import SignUp from './pages/SignUp/index.js';
+import Createclass from './pages/CreateClass/index.js';
+
+import Login from './pages/Login';
+import Personal_info from './pages/personal_info/index.js';
+import ListTutor from './pages/ListTutor/index.js';
+import PendingClass from './pages/PendingClass/PendingClass';
 
 function App() {
     return (
-
         <Layout>
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/classlist" element={<ClassList />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/signup" element={<SignUp />} />
+                <Route path="/create_class" element={<Createclass />} />
+                <Route path="/personal_info" element={<Personal_info />} />
+                <Route path="/tutorlist" element={<ListTutor />} />
 
-                // Admin
                 <Route path="/pendingClass" element={<PendingClass />} />
             </Routes>
-        </Layout >
-
+        </Layout>
     );
 }
 
