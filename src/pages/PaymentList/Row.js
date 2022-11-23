404c3bf1432320b9b786640127eb95c8d55cc547
Phan Hải Hà <haihaphan626@gmail.com>
2022-11-24 06:33:02 +0700

    Update row.js

--- src/pages/PaymentList/Row.js
+++ src/pages/PaymentList/Row.js
@@ -20,37 +20,37 @@ const ClassDetails = (props) => {
     return (
         <>
             <tr className={`${style.modal}`}>
-                <td colSpan="2" className={`${clsx(style.detail)}`}>
+                <td colSpan="3" className={`${clsx(style.detail)}`}>
                     <i className={`fa-regular fa-clock ${style.icon}`}></i>
                     Thời gian: {info.date}
                 </td>
-                <td colSpan="2" className={`${clsx(style.detail)}`}>
+                <td colSpan="3" className={`${clsx(style.detail)}`}>
                     <i className={`fi fi-rr-arrow-square-right ${style.icon}`}></i>
                     Độ dài khoá học: {info.courseLength}
                 </td>
 
             </tr>
             <tr className={`${style.modal}`}>
-                <td colSpan="2" className={`${clsx(style.detail)}`}>
+                <td colSpan="3" className={`${clsx(style.detail)}`}>
                     <i className={`fi fi-rr-globe ${style.icon}`}></i>
                     Hình thức: {info.isOffline === "true" ? "Offline" : "Online"}</td>
-                <td colSpan="2" className={`${clsx(style.detail)}`}>
+                <td colSpan="3" className={`${clsx(style.detail)}`}>
                     <i className={`fi fi-rr-phone-call ${style.icon}`}></i>
                     SĐT: {info.phone}</td>
             </tr>
             <tr className={`${style.modal}`}>
-                <td colSpan="2" className={`${clsx(style.detail)}`}>
+                <td colSpan="3" className={`${clsx(style.detail)}`}>
                     <i className={`fi fi-rr-home ${style.icon}`}></i>
                     Địa chỉ: {info.address}</td>
-                <td colSpan="2" className={`${clsx(style.detail)}`}>
+                <td colSpan="3" className={`${clsx(style.detail)}`}>
                     <i className={`fi fi-rr-flag ${style.icon}`}></i>
                     Lương: {info.salary}</td>
             </tr>
             <tr className={`${style.modal}`}>
-                <td colSpan="2" className={`${clsx(style.detail)}`}>
+                <td colSpan="3" className={`${clsx(style.detail)}`}>
                     <i className={`fi fi-rr-time-forward ${style.icon}`}></i>
                     Thời gian học: {info.studyTime}</td>
-                <td colSpan="2" className={`${clsx(style.detail)}`}>
+                <td colSpan="3" className={`${clsx(style.detail)}`}>
                     <i className={`fi fi-rr-notebook ${style.icon}`}></i>
                     Yêu cầu: {info.requirement}</td>
             </tr>
